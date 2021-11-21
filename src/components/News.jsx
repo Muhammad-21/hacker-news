09d1fbb1f8311ae694cf7d84ea08d04ffa6a7fd1
Muhammad <muhammadmahmudov21@yandex.ru>
2021-11-21 15:49:41 +0300

    [*] update page and up to header

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {ClockCircleOutlined, CommentOutlined, UserOutlined} from '@ant-design/icons';
-import Update from './Update';
+import Update from './updateButton/Update';
+import Back from './back/Back'
 export default function News() {
+    const [scroll,setScroll] = useState(false);
+
+    const componentDidMount = () => {
+        window.addEventListener('scroll', handleScroll);
+    }
+    
+    const componentWillUnmount = () => {
+        window.removeEventListener('scroll', handleScroll);
+    }
+    
+    const handleScroll = () => {
+        if (window.scrollY >= 100  && !scroll) {
+            setScroll(true);
+        } else if (window.scrollY < 100 && scroll) {
+            setScroll(false)
+        }
+    };
+    const goBack = () => {
+        window.scrollTo(document.body.scrollHeight,0);
+    }
     return (
-        <>
+        <div setScroll={setScroll ? componentDidMount() : componentWillUnmount()}>
         <Update/>
+        <Back isScroll={scroll} goBack={goBack}/>
         <div className="news">
             <div className="title">
                 <strong>State Capacity: What Is It, How We Lost It, and How to Get It Back</strong>
@@ -59,6 +81,6 @@ export default function News() {
                 <div><ClockCircleOutlined/> 10:02:47 PM  📅 November 18,2021</div>
             </div>
         </div>
-        </>
+        </div>
     )
 }
