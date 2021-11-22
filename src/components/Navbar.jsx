e62aac939ad453a29c40d4bbd3abe2d1bd1a316d
Muhammad <muhammadmahmudov21@yandex.ru>
2021-11-22 22:40:10 +0300

    [*] navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import { Layout, Menu } from 'antd'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%'}}>
-            <Menu theme="dark" mode="horizontal">
-                    <Link to="/" className="logo">Hacker-News</Link>
+        <Layout.Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+            <Menu theme="dark" mode="horizontal" style={{color:"white", fontSize: "25px"}}>
+                    Hacker-News
             </Menu>
         </Layout.Header>
     )
